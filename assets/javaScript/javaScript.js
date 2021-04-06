49b0dbfe97edbf338773fddf2971852ec2954518
Chris Onions <c.onions101@gmail.com>
2021-04-06 17:37:04 +1000

    added a spotify funtion to request permission to user data and get a code back.

--- assets/javaScript/javaScript.js
+++ assets/javaScript/javaScript.js
@@ -1,3 +1,26 @@
+var loginButton = document.getElementById("loginButton")
+
+const redirectUri = "https://chrisonions.github.io/webdevawesometeam/"
+const clientID = "85942e5b4e564e30b232074bd5b1417d"
+const clientSecret = "7f12ed9c212649dfaa703852a28d551c"
+const authorise = "https://accounts.spotify.com/authorize"
+
+
+function requestAccessToUserData() {
+  var url = authorise;
+  url += "?client_id=" + clientID;
+  url += "&response_type=code";
+  url += "&redirect_uri="; + encodeURI(redirectUri);
+  url += "&show_dialog=True";
+  url += "&playlist-modify-public user-modify-playback-state playlist-modify-private user-library-read playlist-read-collaborative "
+};
+
+loginButton.addEventListener("click", function () {
+  requestAccessToUserData();
+  window.location.href = url;
+});
+
+
 // SEARCH BOX LISTENER:
 // Take input from search 		  box, checkbox, length.
 
